dd987222d1933170e58c2b81edbaa6fa234e561d
Gvo3d <denis.yakimov89@gmail.com>
2016-03-25 20:19:26 +0200

    v.0.3 - working

---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Hello :: Spring Application</title>
+    </head>
+    <body>
+        <h1>Hello - Spring Application</h1>
+        <p>Greetings.</p>
+    </body>
+</html>
---
+++ src/test/java/Test.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.springframework.web.servlet.ModelAndView;
+import springapp.web.HelloController;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/**
+ * Created by Gvozd on 25.03.2016.
+ */
+
+public class Test {
+
+    @org.junit.Test
+    public void tesing() throws ServletException, IOException {
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);
+        Assert.assertEquals("hello",modelAndView.getViewName());
+    }
+}
