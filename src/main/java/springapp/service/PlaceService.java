116f2b349171d9f043567c52dc1619c44ed10e52
Gvo3d <denis.yakimov89@gmail.com>
2016-03-26 14:43:32 +0200

    v.0.9.5 - in progress trying model( dao+impl, service+impl) css + img  jsp's testcase gmaps api pom.xml

---
+++ src/main/java/springapp/service/PlaceService.java
@@ -0,0 +1,16 @@
+package springapp.service;
+
+import springapp.model.Place;
+import java.util.List;
+
+/**
+ * Created by Gvozd on 26.03.2016.
+ */
+public interface PlaceService {
+
+    public void addPlace(Place place);
+
+    public List<Place> listPlace();
+
+    public void removePlace(Integer id);
+}
---
+++ src/main/java/springapp/service/PlaceServiceImpl.java
@@ -0,0 +1,33 @@
+package springapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import springapp.dao.PlaceDAO;
+import springapp.model.Place;
+
+import java.util.List;
+/**
+ * Created by Gvozd on 26.03.2016.
+ */
+@Service
+public class PlaceServiceImpl implements PlaceService {
+
+    @Autowired
+    private PlaceDAO placeDAO;
+
+    @Transactional
+    public void addPlace(Place place) {
+        placeDAO.addPlace(place);
+    }
+
+    @Transactional
+    public List<Place> listPlace() {
+        return placeDAO.listPlace();
+    }
+
+    @Transactional
+    public void removePlace(Integer id) {
+        placeDAO.removePlace(id);
+    }
+}
